5a7efc9de5c7a733a8ed8692fe7fa6270c4adc0d
Huyghe Gabriel <huyghegabriel.sn@gmail.com>
2022-02-24 09:29:28 +0000

    FIRST COMMIT

---
+++ script.js
@@ -0,0 +1,9 @@
+/*document.addEventListener('DOMContentLoaded', function(init) {
+    
+})*/
+
+let recupPrestataires = document.getElementsByClassName('prestataires');
+
+recupPrestataires.addEventListener('onMouseOver', function() {
+    
+})
